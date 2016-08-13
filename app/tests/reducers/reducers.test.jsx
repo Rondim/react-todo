838cdf778d6b03479557b399252c5af79ff781d0
Dmitry Tsydenov <rondimts@gmail.com>
2016-08-13 19:20:05 +0800

    add reducers & tests

---
+++ app/tests/reducers/reducers.test.jsx
@@ -0,0 +1,27 @@
+var expect = require('expect');
+var df = require('deep-freeze-strict');
+var reducers =  require('reducers');
+
+describe('Reducer', () => {
+  describe('searchTextReducer', () => {
+    it('should set search text', () => {
+      var action = {
+        type: 'SET_SEARCH_TEXT',
+        searchText: 'Dog'
+      };
+      var res = reducers.searchTextReducer(df(''), df(action));
+
+      expect(res).toEqual(action.searchText);
+    });
+  });
+
+  describe('showCompletedReducer', () => {
+    it('should flip status', () => {
+      var action = {
+        type: 'TOGGLE_SHOW_COMPLETED'
+      };
+      var res = reducers.showCompletedReducer(df(true), df(action));
+      expect(res).toEqual(false);
+    });
+  });
+});
