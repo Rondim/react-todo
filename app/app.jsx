8135e6b6f96106f9c1ea40f2ca7891f67205de4f
Dmitry Tsydenov <rondimts@gmail.com>
2016-08-15 20:12:31 +0700

    add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -1,9 +1,20 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 var {Route, Router, IndexRoute, hashHistory} = require("react-router");
+
 var TodoApp = require('TodoApp');
 
+var actions = require('actions');
+var store = require('configureStore').configure();
 
+
+store.subscribe(() => {
+  console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('learn redux'));
+store.dispatch(actions.setSearchText('redux'));
+store.dispatch(actions.toggleShowCompleted());
 $(document).foundation();
 // Custom styles
 require('style!css!sass!applicationStyles');
